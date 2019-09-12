a896c828eddb869fecd3e16b49611d308261d923
Fernanda <sarmiento.fernada.jehovana@gmail.com>
2019-09-11 23:40:44 -0400

    Con Spring HATEOAS para que sea REST (Transferencia de estado representacional) osea que no se vuelva a cargar la pagina, esto usando RESTful que hace referencia a un servicio web que implementa la arquitectura REST.

--- tarea_buses/src/main/java/Nomina/Control_Buses.java
+++ tarea_buses/src/main/java/Nomina/Control_Buses.java
@@ -1,7 +1,10 @@
 package Nomina;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +12,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.*;
+
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 class Control_Buses {
@@ -20,25 +28,28 @@ class Control_Buses {
     }
 
     // Aggregate root
-
+/*
     @GetMapping("/employees")
     List<Buses> all() {
         return repository.findAll();
     }
-
+*/
     @PostMapping("/employees")
     Buses newEmployee(@RequestBody Buses newEmployee) {
         return repository.save(newEmployee);
     }
 
     // Single item
-
+/*
    @GetMapping("/buses/{id}")
     Buses one(@PathVariable Long id) {
 
         return repository.findById(id)
                 .orElseThrow(() -> new BusNotFoundException(id));
     }
+*/
+
+
 
     @PutMapping("/buses/{id}")
     Buses replaceEmployee(@RequestBody Buses newBus, @PathVariable Long id) {
@@ -59,4 +70,34 @@ class Control_Buses {
     void deleteEmployee(@PathVariable Long id) {
         repository.deleteById(id);
     }
+
+    //mas
+
+    @GetMapping("/buses/{id}")
+    Resource<Buses> one(@PathVariable Long id) {
+
+        Buses bus = repository.findById(id)
+                .orElseThrow(() -> new BusNotFoundException(id));
+
+        Resource<Buses> busees = new Resource<>(bus,
+                linkTo(methodOn(Control_Buses.class).one(id)).withSelfRel(),
+                linkTo(methodOn(Control_Buses.class).all()).withRel("buses"));
+        return busees;
+    }
+
+    @GetMapping("/buses")
+    Resources<Resource<Buses>> all() {
+
+        List<Resource<Buses>> buses = repository.findAll().stream()
+                .map(bus -> new Resource<>(bus,
+                        linkTo(methodOn(Control_Buses.class).one(bus.getId())).withSelfRel(),
+                        linkTo(methodOn(Control_Buses.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return new Resources<>(buses,
+                linkTo(methodOn(Control_Buses.class).all()).withSelfRel());
+    }
+
+
+
 }
